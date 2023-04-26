163ddaadf7c606fd2f6760522f5ab5c647cf8902
Julianne Maghirang <70534153+Maghirang-Julianne@users.noreply.github.com>
2023-04-25 22:03:48 -0700

    Added basic key press sounds

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class Controls : MonoBehaviour
 {
     private SpriteRenderer sr;
+    private AudioSource audioSource;
     public Sprite ogImage;
     public Sprite pressedImage;
+    public AudioClip keyPressSound;
 
     public KeyCode keyToPress;
+
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.clip = keyPressSound;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(keyToPress))
+        if (Input.GetKeyDown(keyToPress))
         {
             sr.sprite = pressedImage;
+            audioSource.Play();
         }
 
         if (Input.GetKeyUp(keyToPress))
